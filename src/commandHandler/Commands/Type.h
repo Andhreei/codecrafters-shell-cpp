5e0ff8fcf5976b70f063c9a3af217afa0d23498a
Andhreei <cristianmirea@icloud.com>
2026-05-04 21:13:43 +0000

    implemented PATH executable search [skip ci]

--- src/commandHandler/Commands/Type.h
+++ src/commandHandler/Commands/Type.h
@@ -1,5 +1,10 @@
 #pragma once
+#include <filesystem>
 #include <format>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
 
 #include "../Interfaces/BuiltInCommand.h"
 
@@ -10,12 +15,31 @@ namespace CommandHandling {
  */
 class Type : public BuiltInCommand {
   public:
-   inline void execute(const std::vector<std::string>& args) override {
+   void execute(const std::vector<std::string>& args) override {
       CommandHandler& commandHandler = CommandHandler::getInstance();
       for (const auto& arg : args) {
+         bool found{false};
          if (commandHandler.isValidCommand(arg)) {
             std::cout << std::format("{} is a shell builtin\n", arg);
-         } else {
+            break;
+         } else if (std::getenv("PATH") not_eq NULL) {
+            const char* pathEnv = std::getenv("PATH");
+            if (pathEnv != nullptr) {
+               std::string pathStr(pathEnv);
+               std::istringstream iss(pathStr);
+               std::string dir;
+               while (std::getline(iss, dir, ':')) {
+                  std::filesystem::path executable = std::filesystem::path(dir) / arg;
+                  if (std::filesystem::exists(executable) &&
+                      (std::filesystem::status(executable).permissions() & std::filesystem::perms::owner_exec) != std::filesystem::perms::none) {
+                     std::cout << std::format("{} is {}\n", arg, executable.string());
+                     found = true;
+                     break;
+                  }
+               }
+            }
+         }
+         if (not found) {
             std::cout << std::format("{}: not found\n", arg);
          }
       }
